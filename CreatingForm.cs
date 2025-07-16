8fb49be1507a66e2756917802de4a6b60fec2826
SONiQz <adrian.forrester.17@googlemail.com>
2025-07-16 10:08:42 +0100

    first commit

---
+++ CreatingForm.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.AccessControl;
+using System.Windows.Forms;
+
+namespace RLJobCreation_Framework
+{
+    public partial class CreatingForm : Form
+    {
+        private string CompletePath;
+        private string JobNo;
+        private string DriveID;
+        private string RootPath;
+
+        public CreatingForm(string completePath, string jobNo, string driveID, string rootPath)
+        {
+            InitializeComponent();
+
+            // Assign parameters to class-level variables
+            this.CompletePath = completePath;
+            this.JobNo = jobNo;
+            this.DriveID = driveID;
+            this.RootPath = rootPath;
+
+            this.Load += new EventHandler(CreateForm_Load);
+        }
+
+        private void CreateForm_Load(object sender, EventArgs e)
+        {
+
+            string StaticPath = CompletePath + "\\" + JobNo + " - ";
+
+            List<string> pathList = PathList.DirectoriesToCreate();
+
+            string ProjectPath = DriveID + ":\\" + RootPath + "\\";
+
+            foreach (string DirectoryPath in pathList)
+            {
+                try
+                {
+                    // Check if the directory already exists
+                    if (Directory.Exists(StaticPath + DirectoryPath))
+                    {
+                        CreatingBox.AppendText($"Directory '{StaticPath + DirectoryPath}' already exists.\r\n");
+                    }
+                    else
+                    {
+                        // Try to create the directory if it does not exist
+                        DirectoryInfo directoryInfo = Directory.CreateDirectory(StaticPath + DirectoryPath);
+                        DirectorySecurity directorySecurity = directoryInfo.GetAccessControl();
+
+                        FileSystemAccessRule accessRule = new FileSystemAccessRule(
+                            "Domain Users",
+                            FileSystemRights.Read |
+                            FileSystemRights.Write |
+                            FileSystemRights.ExecuteFile,          // Read, Write, and Execute permissions
+                            InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit, // Apply to subfolders and files
+                            PropagationFlags.None,                 // Propagation flags
+                            AccessControlType.Allow
+                         );
+
+                        directorySecurity.AddAccessRule(accessRule);
+                        directoryInfo.SetAccessControl(directorySecurity);
+
+
+                        // Check again if the directory exists after creation
+                        if (Directory.Exists(StaticPath + DirectoryPath))
+                        {
+                            CreatingBox.AppendText($"Directory '{StaticPath + DirectoryPath}' created successfully.\r\n");
+                        }
+                        else
+                        {
+                            CreatingBox.AppendText($"Failed to create directory: {DirectoryPath}\r\n");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Catch any errors and display them in the text box
+                    CreatingBox.AppendText($"Error creating directory '{DirectoryPath}': {ex.Message}\r\n");
+                }
+            }
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreatingForm));
+            this.CreatingBox = new System.Windows.Forms.TextBox();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.CreatePanel = new System.Windows.Forms.Panel();
+            this.CreatePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CreatingBox
+            // 
+            this.CreatingBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CreatingBox.Location = new System.Drawing.Point(0, 0);
+            this.CreatingBox.Multiline = true;
+            this.CreatingBox.Name = "CreatingBox";
+            this.CreatingBox.ReadOnly = true;
+            this.CreatingBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.CreatingBox.Size = new System.Drawing.Size(840, 266);
+            this.CreatingBox.TabIndex = 0;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CloseButton.Location = new System.Drawing.Point(753, 15);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(75, 23);
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // CreatePanel
+            // 
+            this.CreatePanel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.CreatePanel.Controls.Add(this.CloseButton);
+            this.CreatePanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.CreatePanel.Location = new System.Drawing.Point(0, 266);
+            this.CreatePanel.Name = "CreatePanel";
+            this.CreatePanel.Size = new System.Drawing.Size(840, 50);
+            this.CreatePanel.TabIndex = 2;
+            // 
+            // CreatingForm
+            // 
+            this.ClientSize = new System.Drawing.Size(840, 316);
+            this.Controls.Add(this.CreatingBox);
+            this.Controls.Add(this.CreatePanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CreatingForm";
+            this.Text = "Creating Folders";
+            this.CreatePanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        private TextBox CreatingBox;
+        private Button CloseButton;
+        private Panel CreatePanel;
+    }
+}
+
---
+++ NewJob.cs
@@ -0,0 +1,315 @@
+﻿using System;
+
+using System.ComponentModel;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace RLJobCreation_Framework
+{
+
+    public partial class NewJob : Form
+    {
+        public NewJob()
+        {
+            InitializeComponent();
+        }
+
+        private void NewJob_Load(object sender, EventArgs e)
+        {
+        }
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void JobLocationCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void JobLocation_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void JobNo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void AboutButton_Click(object sender, EventArgs e)
+        {
+            var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            MessageBox.Show("Simple App for Creating Folder\n" +
+                            "Structures for RL Projects\n" +
+                            "\n" +
+                            "Written by Adrian Forrester\n" +
+                            "\n" +
+                            "adrian.forrester@robertslimbrick.com\nVersion:" + version + "\n");
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewJob));
+            this.JobNo = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.JobTitle = new System.Windows.Forms.TextBox();
+            this.JobLocationCombo = new System.Windows.Forms.ComboBox();
+            this.AboutButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.CreateButton = new System.Windows.Forms.Button();
+            this.LocationLabel = new System.Windows.Forms.Label();
+            this.JobNoLabel = new System.Windows.Forms.Label();
+            this.JobTitleLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // JobNo
+            // 
+            this.JobNo.Location = new System.Drawing.Point(111, 75);
+            this.JobNo.Name = "JobNo";
+            this.JobNo.Size = new System.Drawing.Size(121, 22);
+            this.JobNo.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // JobTitle
+            // 
+            this.JobTitle.Location = new System.Drawing.Point(111, 116);
+            this.JobTitle.Name = "JobTitle";
+            this.JobTitle.Size = new System.Drawing.Size(235, 22);
+            this.JobTitle.TabIndex = 2;
+            // 
+            // JobLocationCombo
+            // 
+            this.JobLocationCombo.AutoCompleteCustomSource.AddRange(new string[] {
+            "Gloucester",
+            "Newport"});
+            this.JobLocationCombo.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.JobLocationCombo.FormattingEnabled = true;
+            this.JobLocationCombo.Items.AddRange(new object[] {
+            "Gloucester",
+            "Newport"});
+            this.JobLocationCombo.Location = new System.Drawing.Point(111, 32);
+            this.JobLocationCombo.Name = "JobLocationCombo";
+            this.JobLocationCombo.Size = new System.Drawing.Size(121, 24);
+            this.JobLocationCombo.TabIndex = 3;
+            // 
+            // AboutButton
+            // 
+            this.AboutButton.Location = new System.Drawing.Point(12, 15);
+            this.AboutButton.Name = "AboutButton";
+            this.AboutButton.Size = new System.Drawing.Size(75, 23);
+            this.AboutButton.TabIndex = 4;
+            this.AboutButton.Text = "About";
+            this.AboutButton.UseVisualStyleBackColor = true;
+            this.AboutButton.Click += new System.EventHandler(this.AboutButton_Click);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(308, 15);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.TabIndex = 5;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // CreateButton
+            // 
+            this.CreateButton.Location = new System.Drawing.Point(227, 15);
+            this.CreateButton.Name = "CreateButton";
+            this.CreateButton.Size = new System.Drawing.Size(75, 23);
+            this.CreateButton.TabIndex = 6;
+            this.CreateButton.Text = "Create Job";
+            this.CreateButton.UseVisualStyleBackColor = true;
+            this.CreateButton.Click += new System.EventHandler(this.CreateButton_Click);
+            // 
+            // LocationLabel
+            // 
+            this.LocationLabel.AutoSize = true;
+            this.LocationLabel.Location = new System.Drawing.Point(18, 35);
+            this.LocationLabel.Name = "LocationLabel";
+            this.LocationLabel.Size = new System.Drawing.Size(87, 16);
+            this.LocationLabel.TabIndex = 7;
+            this.LocationLabel.Text = "Job Location:";
+            // 
+            // JobNoLabel
+            // 
+            this.JobNoLabel.AutoSize = true;
+            this.JobNoLabel.Location = new System.Drawing.Point(18, 78);
+            this.JobNoLabel.Name = "JobNoLabel";
+            this.JobNoLabel.Size = new System.Drawing.Size(84, 16);
+            this.JobNoLabel.TabIndex = 8;
+            this.JobNoLabel.Text = "Job Number:";
+            // 
+            // JobTitleLabel
+            // 
+            this.JobTitleLabel.AutoSize = true;
+            this.JobTitleLabel.Location = new System.Drawing.Point(18, 119);
+            this.JobTitleLabel.Name = "JobTitleLabel";
+            this.JobTitleLabel.Size = new System.Drawing.Size(62, 16);
+            this.JobTitleLabel.TabIndex = 9;
+            this.JobTitleLabel.Text = "Job Title:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.CancelButton);
+            this.panel1.Controls.Add(this.CreateButton);
+            this.panel1.Controls.Add(this.AboutButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 186);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(395, 50);
+            this.panel1.TabIndex = 10;
+            // 
+            // NewJob
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(395, 236);
+            this.Controls.Add(this.JobTitleLabel);
+            this.Controls.Add(this.JobNoLabel);
+            this.Controls.Add(this.LocationLabel);
+            this.Controls.Add(this.JobLocationCombo);
+            this.Controls.Add(this.JobTitle);
+            this.Controls.Add(this.JobNo);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "NewJob";
+            this.Text = "RL Job Directory Creation Tool";
+            this.Load += new System.EventHandler(this.NewJob_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private TextBox JobNo;
+        private ContextMenuStrip contextMenuStrip1;
+        private IContainer components;
+        private TextBox JobTitle;
+
+
+
+        private ComboBox JobLocationCombo;
+        private Button AboutButton;
+        private new Button CancelButton;
+        private Button CreateButton;
+
+
+        private Label LocationLabel;
+        private Label JobNoLabel;
+        private Label JobTitleLabel;
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            string DriveId = "";
+            string RootPath = "";
+            string JobName = "";
+            string CurrJobNo = JobNo.Text;
+            string CompletePath = "";
+            string SelectedDrive = JobLocationCombo.SelectedItem?.ToString();
+
+
+            if (JobLocationCombo.SelectedItem != null)
+            {
+                if (SelectedDrive == "Gloucester")
+                {
+#if DEBUG
+                    DriveId = "C";
+#else
+                    DriveId = "X";
+#endif
+                }
+                else if (SelectedDrive == "Newport")
+                {
+#if DEBUG
+                    DriveId = "C";
+#else
+                    DriveId = "Y";
+#endif
+                }
+                // Pending Changes
+                //  else if (SelectedDrive == "London"){
+                //      DriveId = "A";
+                //  }
+            }
+            else
+            {
+                MessageBox.Show("No Job Location Set");
+                return;
+            }
+
+            int JobNoLen = CurrJobNo.Length;
+            if (JobNoLen > 0 && JobNoLen <= 4)
+            {
+                RootPath = CurrJobNo.Substring(0, 2);
+                RootPath = RootPath + "--";
+            }
+            else if (JobNoLen >= 5)
+            {
+                RootPath = CurrJobNo.Substring(0, 3);
+                RootPath = RootPath + "00";
+            }
+            else
+            {
+                MessageBox.Show("Job Number Empty");
+                return;
+            }
+
+            if (JobTitle.Text.Length > 0)
+            {
+                string CleanJobName = Regex.Replace(JobTitle.Text, $"[{Regex.Escape(new string(Path.GetInvalidFileNameChars()))}]", "_");
+                JobName = JobNo.Text + " - " + CleanJobName;
+            }
+            else
+            {
+                MessageBox.Show("Job Title Is Empty");
+                return;
+            }
+            CompletePath = DriveId + ":\\" + RootPath + "\\" + JobName;
+            DialogResult result = MessageBox.Show("Confirm Job Path Below:\n" + CompletePath,
+                "Confirm Job Creation",
+                MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Question
+            );
+
+            if (result == DialogResult.OK)
+            {
+                string ProjectPath = DriveId + ":\\" + RootPath + "\\";
+                string WildJobNo = CurrJobNo + "*";
+                if (Directory.Exists(ProjectPath))
+                {
+                    bool DirectoryExists = Directory.GetDirectories(ProjectPath, WildJobNo).Length > 0;
+                    if (DirectoryExists)
+                    {
+                        MessageBox.Show("Job Number Already Exists on System - Please Check Inputs");
+                        return;
+                    }
+                }
+
+                CreatingForm creatingForm = new CreatingForm(CompletePath, JobNo.Text, DriveId, RootPath);
+                creatingForm.Show();
+
+            }
+
+            if (result == DialogResult.Cancel)
+            {
+                MessageBox.Show("Aborting Creation");
+            }
+        }
+
+        private Panel panel1;
+    }
+}
